27e4725b1204ec972ac69233d5129cf66fd7559a
John Alison <johnda102@gmail.com>
2024-07-23 12:12:18 -0400

    Update Keyboards

--- setup.sh
+++ setup.sh
@@ -2,3 +2,5 @@ cd ~/
 ln -s ~/dotfiles/.bashrc
 ln -s ~/dotfiles/.aliases
 ln -s ~/dotfiles/.gitconfig
+ln -s ~/dotfiles/KeyboardLayouts/Jamie.icns ~/Library/Keyboard\ Layouts/Jamie.icns
+ln -s ~/dotfiles/KeyboardLayouts/Jamie.keylayout ~/Library/Keyboard\ Layouts/Jamie.keylayout
