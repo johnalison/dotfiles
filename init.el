0eff2c72b2d6beeef395c561cc5a2d7a607558f3
John Alison <johnda102@gmail.com>
2025-06-04 22:24:31 -0500

    feat(ediff): configure horizontal split for ediff windows

--- init.el
+++ init.el
@@ -181,6 +181,7 @@
 	
 	))
 
+(setq ediff-split-window-function 'split-window-horizontally)
 (setq ediff-window-setup-function 'ediff-setup-windows-plain)
 
 (defun replace-blank-lines-with-true-blanks ()
